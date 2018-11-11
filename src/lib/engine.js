d509f8036aa7c1236c9214736f40777e61daee8a
Alexander Pliskin <alexnetmore@gmail.com>
2018-11-11 22:27:45 +0300

    frienship is winner if draw

--- src/lib/engine.js
+++ src/lib/engine.js
@@ -21,11 +21,18 @@ const hasWon = (symbol, board) =>
     hasWonInRow(symbol, board) || hasWonInColumn(symbol, board) ||
     hasWonInLeftSlant(symbol, board) || hasWonInRightSlant(symbol, board);
 
+const isDraw = board =>
+    board[0][0] !== '' && board[0][1] !== '' && board[0][2] !== '' &&
+    board[1][0] !== '' && board[1][1] !== '' && board[1][2] !== '' &&
+    board[2][0] !== '' && board[2][1] !== '' && board[2][2] !== '';
+
 export const getWinner = board => {
     if (hasWon(X, board)) {
         return '1';
     } else if (hasWon(O, board)) {
         return '2';
+    } else if (isDraw(board)) {
+        return 'friendship';
     }
-    return null;
+        return null;
 };
