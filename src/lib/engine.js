e35a50edbf10a6adbb56c206d5a34dbdf112c0bd
Alexander Pliskin <alexnetmore@gmail.com>
2018-11-12 12:12:26 +0300

    refactor isDraw -> isNowFreeFields

--- src/lib/engine.js
+++ src/lib/engine.js
@@ -21,17 +21,15 @@ const hasWon = (symbol, board) =>
     hasWonInRow(symbol, board) || hasWonInColumn(symbol, board) ||
     hasWonInLeftSlant(symbol, board) || hasWonInRightSlant(symbol, board);
 
-const isDraw = board =>
-    board[0][0] !== '' && board[0][1] !== '' && board[0][2] !== '' &&
-    board[1][0] !== '' && board[1][1] !== '' && board[1][2] !== '' &&
-    board[2][0] !== '' && board[2][1] !== '' && board[2][2] !== '';
+const isNowFreeFields = board => board.map(row => row.filter(el => el !== '').length).reduce((a, b) => a + b, 0) === 9;
 
 export const getWinner = board => {
+    console.log(isNowFreeFields(board));
     if (hasWon(X, board)) {
         return '1';
     } else if (hasWon(O, board)) {
         return '2';
-    } else if (isDraw(board)) {
+    } else if (isNowFreeFields(board)) {
         return 'friendship';
     }
         return null;
